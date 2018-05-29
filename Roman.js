a20cfdff1e81982b6e4ed61c26fc9d306c08430c
Jerzy Wickowski <jerzy.wickowski@gmail.com>
2018-05-29 12:13:30 +0200

    pass test for IV

--- Roman.js
+++ Roman.js
@@ -5,8 +5,20 @@ var Roman = {};
         var result = 0;
         for(var i = 0; i< arabicNumber.length; i++){
             var current = arabicNumber[i];
+            var isNext = i+1 < arabicNumber.length;
             if(current === "I"){
+                if(isNext){
+                    if(arabicNumber[i+1] == "V"){
+                        result --;
+                    }
+                    else{
+                        result ++
+                    }
+                }
+                else{
                 result ++;
+            }
+            
             }
             if(current === "V"){
                 result += 5;
